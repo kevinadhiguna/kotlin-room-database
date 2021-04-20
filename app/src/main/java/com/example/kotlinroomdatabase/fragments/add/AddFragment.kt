5333e6db76506e374ad7b0a7af31f8d14e909992
kevinadhiguna <kevin26798@gmail.com>
2021-04-21 05:56:06 +0700

    chore: reorganize packages

--- app/src/main/java/com/example/kotlinroomdatabase/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/fragments/add/AddFragment.kt
@@ -1,19 +1,20 @@
-package com.example.kotlinroomdatabase
+package com.example.kotlinroomdatabase.fragments.add
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.kotlinroomdatabase.R
 
-class ListFragment : Fragment() {
+class AddFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        return inflater.inflate(R.layout.fragment_add, container, false)
     }
 
 }
--- app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListFragment.kt
@@ -1,19 +1,20 @@
-package com.example.kotlinroomdatabase
+package com.example.kotlinroomdatabase.fragments.list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.kotlinroomdatabase.R
 
-class AddFragment : Fragment() {
+class ListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
 }
