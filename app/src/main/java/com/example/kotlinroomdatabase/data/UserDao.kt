2b090f51f744de4870876cf531dad7c881b7f40f
kevinadhiguna <kevin26798@gmail.com>
2021-04-21 00:49:15 +0700

    refactor: add User data access object

---
+++ app/src/main/java/com/example/kotlinroomdatabase/data/UserDao.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinroomdatabase.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+// UserDao contains the methods used for accessing the database, including queries.
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE) // <- Annotate the 'addUser' function below. Set the onConflict strategy to IGNORE so if exactly the same user exists, it will just ignore it.
+    suspend fun addUser(user: User)
+
+    @Query("SELECT * from user_table ORDER BY id ASC") // <- Add a query to fetch all users (in user_table) in ascending order by their IDs.
+    fun readAllData(): LiveData<List<User>> // <- This means function return type is List. Specifically, a List of Users.
+}
