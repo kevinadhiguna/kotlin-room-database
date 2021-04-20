78d1fba52b846d235dd021c88d46f055a640dc68
kevinadhiguna <kevin26798@gmail.com>
2021-04-21 01:16:35 +0700

    refactor: add user viewModel

---
+++ app/src/main/java/com/example/kotlinroomdatabase/data/UserViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.kotlinroomdatabase.data
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+// UserViewModel provides users data to the UI and survive configuration changes.
+// A ViewModel acts as a communication center between the Repository and the UI.
+
+class UserViewModel(application: Application): AndroidViewModel(application) {
+    private val readAllData: LiveData<List<User>>
+    private val repository: UserRepository
+
+    init {
+        val userDao = UserDatabase.getDatabase(application).userDao()
+        repository= UserRepository(userDao)
+        readAllData = repository.readAllData
+    }
+
+    fun addUser(user: User) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.addUser(user)
+        }
+    }
+}
