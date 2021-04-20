2885bf2ed1bb0e684e6df372c04eff988c83f084
kevinadhiguna <kevin26798@gmail.com>
2021-04-21 01:04:34 +0700

    refactor: add user database

---
+++ app/src/main/java/com/example/kotlinroomdatabase/data/UserDatabase.kt
@@ -0,0 +1,39 @@
+package com.example.kotlinroomdatabase.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// UserDatabase represents database and contains the database holder and server the main access point for the underlying connection to your app's persisted, relational data.
+
+@Database(
+    entities = [User::class],
+    version = 1,                // <- Database version
+    exportSchema = false
+)
+abstract class UserDatabase: RoomDatabase() { // <- Add 'abstract' keyword and extends RoomDatabase
+    abstract fun userDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: UserDatabase? = null
+
+        fun getDatabase(context: Context): UserDatabase{
+            val tempInstance = INSTANCE
+
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    UserDatabase::class.java,
+                    "user_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
