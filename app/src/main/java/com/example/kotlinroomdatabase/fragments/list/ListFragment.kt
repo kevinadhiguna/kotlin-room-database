54e01332eb94f68e51a846e503565347322bb4f2
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-22 06:29:02 +0700

    refactor: revert to initial code

--- app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListFragment.kt
@@ -14,11 +14,7 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_list, container, false)
-
-
-
-        return view
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
 }
