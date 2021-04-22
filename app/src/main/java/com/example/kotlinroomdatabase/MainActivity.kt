d452c7c1f33a35e0ac27c0ea6ce9919b6c47896b
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-22 07:19:13 +0700

    refactor: set up viewBinding in MainActivity

--- app/src/main/java/com/example/kotlinroomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.kotlinroomdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlinroomdatabase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
