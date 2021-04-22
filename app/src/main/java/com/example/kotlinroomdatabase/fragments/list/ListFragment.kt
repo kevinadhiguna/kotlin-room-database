9f06b3b3e1bc1010dd4709d4ef7572e39b76cfe0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-22 07:15:45 +0700

    fix: use viewBinding and remove unused code

--- app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListFragment.kt
@@ -6,15 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.kotlinroomdatabase.R
+import com.example.kotlinroomdatabase.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+
+        binding.floatingActionButton
+
+        return binding.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null // <- whenever we destroy our fragment, _binding is set to null. Hence it will avoid memory leaks.
+    }
+
+
 
 }
