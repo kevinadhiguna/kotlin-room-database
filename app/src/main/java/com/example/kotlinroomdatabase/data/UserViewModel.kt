386f641bd9e816ecbf867e4f63c1e8cbff06d0bd
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-26 20:16:02 +0700

    refactor: remove private modifier on readAllData

--- app/src/main/java/com/example/kotlinroomdatabase/data/UserViewModel.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/data/UserViewModel.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.launch
 // A ViewModel acts as a communication center between the Repository and the UI.
 
 class UserViewModel(application: Application): AndroidViewModel(application) {
-    private val readAllData: LiveData<List<User>>
+    val readAllData: LiveData<List<User>>
     private val repository: UserRepository
 
     init {
