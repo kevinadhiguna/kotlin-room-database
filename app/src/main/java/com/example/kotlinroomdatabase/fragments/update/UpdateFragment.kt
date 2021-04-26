93387bed2adcb1d50c9b56b8c4ea2d2971ff080d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-26 22:42:52 +0700

    refactor: add findViewById to each input column

--- app/src/main/java/com/example/kotlinroomdatabase/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/fragments/update/UpdateFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.navigation.fragment.navArgs
 import com.example.kotlinroomdatabase.R
 
 class UpdateFragment : Fragment() {
+
+    // private var _binding: FragmentAddBinding? = null
+    // private val binding get() = _binding!!
 
     private val args by navArgs<UpdateFragmentArgs>()
 
@@ -16,11 +20,16 @@ class UpdateFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        // _binding = FragmentAddBinding.inflate(inflater, container, false)
+
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_update, container, false)
 
-
+        view.findViewById<TextView>(R.id.updateFirstName_et).setText(args.currentUser.firstName)
+        view.findViewById<TextView>(R.id.updateLastName_et).setText(args.currentUser.lastName)
+        view.findViewById<TextView>(R.id.updateAge_et).setText(args.currentUser.age.toString())
 
         return view
+        // return binding.root
     }
 }
