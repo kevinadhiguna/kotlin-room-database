b89e9cd36cdff864da1410ebe09847db0dc78db1
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-26 23:15:38 +0700

    refactor add updateUser function

--- app/src/main/java/com/example/kotlinroomdatabase/repository/UserRepository.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/repository/UserRepository.kt
@@ -12,4 +12,8 @@ class UserRepository(private val userDao: UserDao) {
     suspend fun addUser(user: User) {
         userDao.addUser(user)
     }
+
+    suspend fun updateUser(user: User) {
+        userDao.updateUser(user)
+    }
 }
