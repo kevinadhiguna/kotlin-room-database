c8b5811245c9dad995372c5d7fa53e56434f71f9
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-26 21:58:38 +0700

    refactor: initialize onClickListener on rowLayout

--- app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListAdapter.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinroomdatabase.R
 import com.example.kotlinroomdatabase.model.User
@@ -31,6 +32,10 @@ class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
         holder.itemView.findViewById<TextView>(R.id.firstName_txt).text = currentItem.firstName
         holder.itemView.findViewById<TextView>(R.id.lastName_txt).text = currentItem.lastName
         holder.itemView.findViewById<TextView>(R.id.age_txt).text = currentItem.age.toString()
+
+        holder.itemView.findViewById<ConstraintLayout>(R.id.rowLayout).setOnClickListener {
+            val action = ListFragmentDirections // <- Pass object to Update Fragment
+        }
     }
 
     fun setData(user: List<User>) {
