89df425fb0b733ee60aaaab992e6337d6ec41e48
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-26 15:13:47 +0700

    refactor: add return values to each functions except onBindViewHolder

--- app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListAdapter.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListAdapter.kt
@@ -1,24 +1,35 @@
 package com.example.kotlinroomdatabase.fragments.list
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlinroomdatabase.R
+import com.example.kotlinroomdatabase.data.User
 
 class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    private var userList = emptyList<User>()
 
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row, parent, false))
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return userList.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentItem = userList[position]
+
+        holder.itemView.findViewById<TextView>(R.id.id_txt).text = currentItem.id.toString()
+        holder.itemView.findViewById<TextView>(R.id.firstName_txt).text = currentItem.firstName
+        holder.itemView.findViewById<TextView>(R.id.lastName_txt).text = currentItem.lastName
+        holder.itemView.findViewById<TextView>(R.id.age_txt).text = currentItem.age.toString()
     }
 }
