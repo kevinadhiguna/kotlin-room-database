96d9b05b9f3f3774858cd7bc6ccdbcdc15e84d6c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-27 06:42:01 +0700

    refactor(Query): add DELETE query

--- app/src/main/java/com/example/kotlinroomdatabase/data/UserDao.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/data/UserDao.kt
@@ -14,6 +14,12 @@ interface UserDao {
     @Update
     suspend fun updateUser(user: User)
 
+    @Delete
+    suspend fun deleteUser(user: User)
+
+    @Query("DELETE FROM user_table")
+    suspend fun deleteAllUsers()
+
     @Query("SELECT * from user_table ORDER BY id ASC") // <- Add a query to fetch all users (in user_table) in ascending order by their IDs.
     fun readAllData(): LiveData<List<User>> // <- This means function return type is List. Specifically, a List of Users.
 }
