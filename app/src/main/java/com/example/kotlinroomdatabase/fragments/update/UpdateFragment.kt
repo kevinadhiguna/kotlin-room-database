e426360e00217f4877c7493bbae10fe7c42ad2ee
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-26 22:06:50 +0700

    refactor: add args

--- app/src/main/java/com/example/kotlinroomdatabase/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/fragments/update/UpdateFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.kotlinroomdatabase.R
 
 class UpdateFragment : Fragment() {
+
+    private val args by navArgs<UpdateFragmentArgs>()
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        val view = inflater.inflate(R.layout.fragment_update, container, false)
+
+
+
+        return view
     }
 }
