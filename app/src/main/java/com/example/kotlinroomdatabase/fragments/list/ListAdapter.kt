6fa832195e02b93005a29ecd7179c459fb77727d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-26 15:06:15 +0700

    refactor: add MyViewHolderClass and some other functions

--- app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListAdapter.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.kotlinroomdatabase.fragments.list
 
-class ListAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
