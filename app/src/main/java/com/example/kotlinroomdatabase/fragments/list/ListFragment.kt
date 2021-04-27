9a35ff47e047234914d55c4391bbae90b3567fa4
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-27 07:17:34 +0700

    refactor: initialize onCreateOptionsMenu function

--- app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/kotlinroomdatabase/fragments/list/ListFragment.kt
@@ -52,7 +52,7 @@ class ListFragment : Fragment() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.delete_menu, menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
